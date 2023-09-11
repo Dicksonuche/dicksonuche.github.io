4edf6603d28fc6988557ac7193e5534dea167a5c
Dicksonuche <144337376+Dicksonuche@users.noreply.github.com>
2023-09-11 01:27:21 +0100

    Delete Task One directory

--- Task One/Date.js
+++
@@ -1,5 +0,0 @@
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const currentDayIndex = new Date().getDay();
-const currentDayName = daysOfWeek[currentDayIndex];
-const currentDayElement = document.getElementById('currentDay');
-currentDayElement.textContent = currentDayName;
